be13ce0bc7e9f9eaabdba9cef16e46a513beb9fd
Hannes Rantzsch <gh@hannesrantzsch.de>
2017-08-06 14:15:55 +0200

    started refactoring

--- pyml_badge.py
+++ pyml_badge.py
@@ -1,22 +1,38 @@
-import ugfx, gc, wifi, badge
+import ugfx
+import badge
+import wifi
 from time import sleep
-import urequests as requests
 
 import usocket as socket
-import _thread as thread
+
+
+state_map = {
+    'up': 0,
+    'down': 1,
+    'left': 2,
+    'right': 3,
+    'a': 4,
+    'b': 5,
+    'start': 8,
+    'select': 9,
+}
+states = [0 for _ in range(14)]
 
 
 def init_badge():
+    badge.init()
     ugfx.init()
 
-    # Make sure WiFi is connected
     wifi.init()
     show_message("Waiting for wifi...")
 
     # Wait for WiFi connection
+    i = 0
     while not wifi.sta_if.isconnected():
-        sleep(0.1)
-        pass
+        show_message('Waiting: {}'.format(i))
+        sleep(0.3)
+        i += 1
+    show_message("Connected")
 
 
 def show_message(message):
@@ -25,148 +41,119 @@ def show_message(message):
     ugfx.flush()
 
 
-class Communicator:
+class Connection:
 
-    def __init__(self, host, port, listener_port):
-        self.host = host
-        self.port = port
-        self.listener_port = listener_port
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
-        self.destination_address = socket.getaddrinfo(self.host, self.port)[0][-1]
+    def __init__(self, listen_port, control_addr, control_port):
         self.uid = None
 
-    def set_uid(self, uid):
-        self.uid = uid
-        thread.start_new_thread(self.ping, (1,))
+        self.listen_port = listen_port
+        self.listen_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+        self.listen_sock.setblocking(False)
+        # self.listen_sock.bind(('0.0.0.0', self.listen_port))
+        addr = socket.getaddrinfo('0.0.0.0', listen_port)
+        self.listen_sock.bind(addr[0][-1])
+
+        self.control_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+        self.control_dest = socket.getaddrinfo(control_addr, control_port)[0][-1]
+
+        self.register()
+
+    def ready(self):
+        return self.uid is not None
+
+    def register(self):
+        command = '/controller/new/{port}'.format(port=self.listen_port)
+        try:
+            self.control_sock.sendto(command.encode('utf-8'), self.control_dest)
+        except Exception as ex:
+            print("failed to register controller: {}".format(ex))
+
+    def handle_read(self, data):
+        data = data.decode('utf-8')
+        if '/' not in data:  # bad, malicous data!!
+            return
+
+        command, data = data.rsplit('/', 1)
+        if command.startswith('/uid'):
+            self.handle_uid(data)
+        elif command.startswith('/rumble'):
+            # self.handle_rumble(data)
+            pass
+        elif command.startswith('/message'):
+            # self.handle_message(data)
+            pass
+        elif command.startswith('/download'):
+            # self.handle_download(data)
+            pass
+        elif command.startswith('/play'):
+            # self.handle_play(data)
+            pass
+
+    def handle_uid(self, data):
+        self.uid = data
+        self.init_inputs()
+
+    def start_listening(self):
+        self.listening = True
+        self._listener_loop()
+
+    def stop_listening(self):
+        self.listening = False
+
+    def _listener_loop(self):
+        while self.listening:
+            try:
+                data, addr = self.listen_sock.recvfrom(1024)
+                self.handle_read(data)
+            except:
+                pass
+            sleep(0.01)
+
+    def handle_btn(self, btn_id, pressed):
+        print("button pressed: {}".format(btn_id))
+        states[btn_id] = int(pressed)
+        self.send_key_states(states)
+
+    def init_inputs(self):
+        print("initializing input callbacks")
+        ugfx.input_init()
+        ugfx.input_attach(ugfx.JOY_UP, lambda pressed: handle_btn(state_map['up'], pressed))
+        ugfx.input_attach(ugfx.JOY_DOWN, lambda pressed: handle_btn(state_map['down'], pressed))
+        ugfx.input_attach(ugfx.JOY_LEFT, lambda pressed: handle_btn(state_map['left'], pressed))
+        ugfx.input_attach(ugfx.JOY_RIGHT, lambda pressed: handle_btn(state_map['right'], pressed))
+        ugfx.input_attach(ugfx.BTN_A, lambda pressed: handle_btn(state_map['a'], pressed))
+        ugfx.input_attach(ugfx.BTN_B, lambda pressed: handle_btn(state_map['b'], pressed))
+        ugfx.input_attach(ugfx.BTN_SELECT, lambda pressed: handle_btn(state_map['select'], pressed))
+        ugfx.input_attach(ugfx.BTN_START, lambda pressed: handle_btn(state_map['start'], pressed))
+
+    def ping(self):
+        command = '/controller/{uid}/ping/{port}'.format(uid=self.uid,
+                                                         port=self.port)
+        socket.sendto(command.encode('utf-8'), self.control_dest)
 
     def send_key_states(self, states):
         command = '/controller/{uid}/states/{states}'.format(
-            uid=self.uid,
-            states=''.join(map(str, states)),
-        )
-        self.socket.sendto(command.encode('utf-8'), self.destination_address)
+                uid=self.uid, states=''.join(map(str, states)))
+        try:
+            self.control_sock.sendto(command.encode('utf-8'), self.control_dest)
+        except Exception as e:
+            print(str(e))
 
-    def register(self):
-        command = '/controller/new/{port}'.format(
-            port=self.listener_port,
-        )
-        self.socket.sendto(command.encode('utf-8'), self.destination_address)
-
-    def ping(self, arg):
-        command = '/controller/{uid}/ping/{port}'.format(
-            uid=self.uid,
-            port=self.listener_port,
-        )
-        while True:
-            self.socket.sendto(command.encode('utf-8'), self.destination_address)
-            sleep(20)
-
-
-class Listener:
-
-    def __init__(self, port, set_uid_callback):
-        self.port = port
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
-        addrinfo = socket.getaddrinfo('0.0.0.0', port)
-        self.socket.bind(addrinfo[0][-1])
-        self.uid = None
-        self.set_uid_callback = set_uid_callback
-
-    def control_loop(self, arg):
-        while True:
-            data, addr = self.socket.recvfrom(1024)
-            data = data.decode('utf-8')
-
-            if '/' not in data:
-                # bad, malicous data!!
-                continue
-
-            command, data = data.rsplit('/', 1)
-            if command.startswith('/uid'):
-                print("got uid")
-                self.handle_uid(data)
-            elif command.startswith('/rumble'):
-                self.handle_rumble(data)
-            elif command.startswith('/message'):
-                self.handle_message(data)
-            elif command.startswith('/download'):
-                self.handle_download(data)
-            elif command.startswith('/play'):
-                self.handle_play(data)
-
-    def handle_uid(self, uid):
-        show_message("Got UID: {}".format(uid))
-        self.uid = uid
-        self.set_uid_callback(uid)
-
-    def handle_rumble(self, duration):
-        show_message("'rumble' is not yet implemented")
-
-    def handle_message(self, message):
-        show_message(message)
-
-    def handle_download(self, url):
-        show_message("'download' is not yet implemented")
-
-    def handle_file(self, file):
-        show_message("'file' is not yet implemented")
-
-
-class InputHandler:
-
-    state_map = {
-        'up': 0,
-        'down': 1,
-        'left': 2,
-        'right': 3,
-        'a': 4,
-        'b': 5,
-        'start': 8,
-        'select': 9,
-    }
-
-    def __init__(self, controller):
-        self.states = [0 for _ in range(14)]
-        self.controller = controller
-        self.shutdown = False
-        self.init_inputs(1)
-
-    def init_inputs(self, arg):
-        ugfx.input_init()
-        ugfx.input_attach(ugfx.JOY_UP, lambda pressed: self.handle_btn(self.state_map['up'], pressed))
-        ugfx.input_attach(ugfx.JOY_DOWN, lambda pressed: self.handle_btn(self.state_map['down'], pressed))
-        ugfx.input_attach(ugfx.JOY_LEFT, lambda pressed: self.handle_btn(self.state_map['left'], pressed))
-        ugfx.input_attach(ugfx.JOY_RIGHT, lambda pressed: self.handle_btn(self.state_map['right'], pressed))
-        ugfx.input_attach(ugfx.BTN_A, lambda pressed: self.handle_btn(self.state_map['a'], pressed))
-        ugfx.input_attach(ugfx.BTN_B, lambda pressed: self.handle_btn(self.state_map['b'], pressed))
-        ugfx.input_attach(ugfx.BTN_SELECT, lambda pressed: self.handle_btn(self.state_map['select'], pressed))
-        ugfx.input_attach(ugfx.BTN_START, lambda pressed: self.handle_btn(self.state_map['start'], pressed))
-        thread.start_new_thread(self.start_pushing, (1,))
-
-    def start_pushing(self, arg):
-        while True:
-            if self.controller.uid is not None:
-                self.controller.send_key_states(self.states)
-            if self.shutdown:
-                break
-            sleep(0.05)
-
-    def handle_btn(self, btn_id, pressed):
-        self.states[btn_id] = int(pressed)
-        self.controller.send_key_states(self.states)
 
 def main():
-
     init_badge()
 
-    communicator = Communicator('127.0.0.1', 1338, 1339)
-    listener = Listener(1339, communicator.set_uid)
-    communicator.register()
-    thread.start_new_thread(listener.control_loop, (1,))
-    input_handler = InputHandler(communicator)
+    destination = 'control.ilexlux.xyz'
+    show_message("Connecting to {}".format(destination))
 
-    while True:
-        sleep(0.01)
+    connection = Connection(1339, destination, 1338)
+    connection.start_listening()
+
+    while not connection.ready():
+        sleep(0.2)
+
+    show_message("Got uid: {}\nInitializing inputs...".format(connection.uid))
+    connection.init_inputs()
 
 
 main()
